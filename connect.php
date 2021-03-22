b6ebbad9f4c96db7a8544107e77a86fc3179c311
leslie <lesloo@protonmail.com>
2021-03-22 11:21:14 +0100

    owa owa

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+    $dbn = 'mysql:dbname=dynamischecharacters;host=localhost';
+    $user = 'root';
+    $password = 'mysql';
+    
+    $con = new PDO($dbn, $user, $password);
+?>
---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+    include 'connect.php';
+    $sql = 'SELECT * FROM characters ORDER BY name';
+    $result = $con->query($sql);
+
+    
+    $sql = 'SELECT COUNT(id) AS aantal FROM characters';
+    $count = $con->prepare($sql);
+    $count->execute();
+    $count = $count->fetchAll();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>All Characters</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link href="resources/css/style.css" rel="stylesheet"/>
+</head>
+<body>
+<header><h1>Alle <?php echo $count[0]['aantal']?> characters uit de database</h1>
+
+</header>
+<?php 
+    foreach($result as $row){
+?>
+<div id="container">
+    <a class="item" href="character.html">
+        <div class="left">
+            <img class="avatar" src="images/<?php echo $row['avatar']?>">
+        </div>
+        <div class="right">
+            <h2><?php echo $row['name']?></h2>
+            <div class="stats">
+                <ul class="fa-ul">
+                    <li><span class="fa-li"><i class="fas fa-heart"></i></span><?php echo $row['health']?></li>
+                    <li><span class="fa-li"><i class="fas fa-fist-raised"></i></span><?php echo $row['attack']?></li>
+                    <li><span class="fa-li"><i class="fas fa-shield-alt"></i></span><?php echo $row['defense']?></li>
+                </ul>
+            </div>
+        </div>
+        <div class="detailButton"><i class="fas fa-search"></i> bekijk</div>
+    </a>
+</div>
+<?php }?>
+<footer>&copy; Leslie 2020</footer>
+</body>
+</html>
